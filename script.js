ca0a61c9fadd2fb0d610c1c75bc60ce5d9767e6b
surajgaria <surajsingh3097@gmail.com>
2024-03-18 06:52:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let cursor = document.querySelector('.cursor');
+let cursorBlur = document.querySelector('.cursor-blur');
+
+document.addEventListener('mousemove', function (dets) {
+    cursor.style.left = dets.x + 'px';
+    cursor.style.top = dets.y + 'px';
+    cursorBlur.style.left = dets.x - 200 + 'px';
+    cursorBlur.style.top = dets.y - 200 + 'px';
+})
+
+
+
+
+gsap.to('.nav', {
+    height: '90px',
+    backgroundColor: '#000',
+    duration: 0.5,
+    scrollTrigger: {
+        trigger: '.nav',
+        scroller: 'body',
+        markers: true,
+        start: 'top -10%',
+        end: 'top -11%',
+        scrub: 2
+    }
+})
+
+gsap.to('.main', {
+    backgroundColor: '#000',
+    scrollTrigger: {
+        trigger: '.main',
+        scroller: 'body',
+        markers: true,
+        scrub: 2,
+        start: 'top -30%',
+        end: 'top -80%'
+    }
+})
